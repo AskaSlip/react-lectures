2ee64d7a462e043217c81b3e9d049d7a7daaeec6
AskaSlip <varenaska@gmail.com>
2024-07-01 22:02:19 +0400

    consult 2 example from the video

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
 
+  const [skipValue, setSkipValue] = useState<number>(0)
+  const [todos, setTodos] = useState<any[]>([])
+  
+  useEffect(() => {
+    fetch('https://dummyjson.com/todos?limit=5&skip=' + skipValue)
+        .then(value => value.json())
+        .then(todosResponse => {
+          setTodos(todosResponse.todos)
+        })
+  }, [skipValue]);
+
+
   return (
-    <div></div>
+      <div>
+
+        <ul>
+          {todos.map(value => <li key={value.id}>{value.todo}</li>)}
+        </ul>
+
+        <hr/>
+
+        <button onClick={() => {
+          console.log('prev');
+          setSkipValue(skipValue -10);
+        }}>
+          prev
+        </button>
+
+        <button onClick={() => {
+        console.log('next');
+        setSkipValue(skipValue +10)
+        }}>
+          next
+        </button>
+
+
+      </div>
   );
 }
 
